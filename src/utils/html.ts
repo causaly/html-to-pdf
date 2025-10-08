0dd1040961d591e352789d65498f98918d5cdf36
Mark Goodwin <mark.goodwin@causaly.com>
2025-10-08 16:40:17 +0100

    fix: relax CSP for a permissive default

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -2,7 +2,7 @@ import { default as sanitizeHtmlCommand } from 'sanitize-html';
 
 export function wrapHtmlWithCSP(html: string): string {
   const cspPolicy =
-    "default-src 'self'; script-src 'self' https://*.tailwindcss.com 'sha256-GYOeRN4LL+IyzNeGMYGAeRyXME5PWdnz8JAfEmhv0E0='; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' https://cdn.tailwindcss.com https://fonts.gstatic.com; connect-src 'none'; frame-src 'none'; object-src 'none'; base-uri 'self'";
+    "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';";
 
   return `<!DOCTYPE html>
 <html>
